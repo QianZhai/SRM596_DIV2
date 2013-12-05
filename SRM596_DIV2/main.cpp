46c8ee42bb5d5b8a9af5f1d63894ba069d318e33
ipuser <ipuser@ipmac-35.local>
2013-12-05 17:12:47 +0900

    Initial Commit

---
+++ SRM596_DIV2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SRM596_DIV2
+//
+//  Created by ipuser on 13/12/05.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
